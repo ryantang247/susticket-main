420afccf0a29159f32f365d98976fae24b98f3e0
ryantang <ryantang_register@hotmail.com>
2024-05-23 20:01:15 +0800

    Minor tweak in nuxt config files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,61 +1,35 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-    nitro: {
-        preset: 'vercel-edge',
-    },
     routeRules: {
         '/': { prerender: true },
     },
-    // Uncomment this during production build
     build:{
         transpile: ['veaury'],
     },
-  runtimeConfig:{
+    runtimeConfig:{
 
-      public:{
-          workspaceKey: process.env.workspaceKey,
-          chatProjectId: process.env.chatProjectId,
-          chatPrivateKey: process.env.chatPrivateKey,
-          reccPublicKey: process.env.reccPublicKey
-      } ,
-      // workspaceKey: process.env.workspaceKey,
-      // chatProjectId: process.env.chatProjectId,
-      // chatPrivateKey: process.env.chatPrivateKey,
-      // reccPublicKey: process.env.reccPublicKey
+        public:{
+            workspaceKey: process.env.workspaceKey,
+            chatProjectId: process.env.chatProjectId,
+            chatPrivateKey: process.env.chatPrivateKey,
+            reccPublicKey: process.env.reccPublicKey
+        } ,
 
-  },
-  modules: [
-    '@element-plus/nuxt',
-    '@nuxt/test-utils/module'
-    // 'nuxt-icon'
-  ],
-  devtools: { enabled: true },
+    },
+    modules: [
+        '@element-plus/nuxt',
+        '@nuxt/test-utils/module'
+        // 'nuxt-icon'
+    ],
+    devtools: { enabled: true },
     vite: {
         // plugins: [
         //     veaury
-        // ],
+        // ]
         build: {
-            // Uncomment this in commiting this to produciton
             commonjsOptions: {
                 transformMixedEsModules: true,
             },
         }
     }
 })
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// import { defineNuxtConfig } from 'nuxt'
-
-// export default defineNuxtConfig({
-//   modules: [
-//     '@element-plus/nuxt'
-//   ],
-//   plugins: [
-//     '~/plugins/fontawesome.ts'
-//   ],
-//   devtools: { enabled: true }
-// })
-
-
-
