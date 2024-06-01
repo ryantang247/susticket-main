7cb4f8d90da5543c818588105d16370ef27ca6f0
MaClazz <sustech.file@gmail.com>
2024-06-01 15:54:40 +0800

    run notif

--- tests/notification.spec.ts
+++ tests/notification.spec.ts
@@ -1,23 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  // Recording...
   await page.goto('https://susticket-main.vercel.app/');
-  await page.getByRole('img', { name: 'logo' }).click();
-  await page.getByRole('banner').getByRole('img').nth(1).click();
+  await page.locator('div').filter({ hasText: /^Notification$/ }).click();
+  await page.getByRole('img', { name: '~/assets/header/' }).click();
   await page.getByRole('button', { name: 'Log out' }).click();
+  await page.getByRole('button', { name: 'Login' }).click();
   await page.getByPlaceholder('Student/Faculty ID').click();
   await page.getByPlaceholder('Student/Faculty ID').fill('12111428');
   await page.getByPlaceholder('Password').click();
   await page.getByPlaceholder('Password').fill('12111428');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('banner').click();
-  await page.getByRole('banner').click();
-  await page.getByRole('banner').click();
-  await page.getByRole('banner').click({
-    clickCount: 3
-  });
-  await page.getByRole('banner').getByRole('img').nth(1).click();
-  await page.getByText('Hello, Ryan!What event are').click();
+  await page.getByRole('button', { name: 'Notification' }).click();
+  await page.locator('div').filter({ hasText: /^Hello SE ProjectThis is week 15\. Enjoy\.\.\.\.2024-05-28 22:32:49$/ }).locator('h3').click();
+  await page.locator('div').filter({ hasText: /^Hello SE ProjectThis is week 15\. Enjoy\.\.\.\.2024-05-28 22:32:49$/ }).getByRole('paragraph').click();
+  await page.getByRole('heading', { name: 'Hihi jsut to let you know' }).click();
+  await page.getByText('Yow').click();
+  await page.getByRole('heading', { name: 'We are haivng a presentation' }).click();
+  await page.getByText('Yeah').click();
   await page.locator('div').filter({ hasText: /^11$/ }).getByRole('img').click();
-  await expect(page.getByRole('banner')).toContainText('Hello SE Project');
+  await page.getByText('11', { exact: true }).click();
 });
